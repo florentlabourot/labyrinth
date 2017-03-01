62ee7a73eca7cc87ee43286cada044f6ea878aa1
flabourot <flabourot@cityway.fr>
2017-03-01 16:56:58 +0100

    extend hashmap

---
+++ org.labyrinthes.generator/src/main/java/org/labyrinthes/common/model/BasicParam.java
@@ -0,0 +1,8 @@
+package org.labyrinthes.common.model;
+
+import java.util.HashMap;
+import java.util.Properties;
+
+public class BasicParam extends HashMap {
+	
+}
--- org.labyrinthes.generator/src/main/java/org/labyrinthes/common/model/InputParam.java
+++ org.labyrinthes.generator/src/main/java/org/labyrinthes/common/model/InputParam.java
@@ -2,6 +2,28 @@ package org.labyrinthes.common.model;
 
 import org.labyrinthes.common.CommandArg;
 
-public class InputParam implements CommandArg{
+public class InputParam extends BasicParam implements CommandArg{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -948354318617495688L;
+	
+	private int width;
+	private int height;
+	
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
 
 }
